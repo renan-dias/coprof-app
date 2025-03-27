fa618a15a4072ee6662e0ba41527a0726d285586
renan-dias <renanprodias2050@gmail.com>
2025-03-27 14:10:52 -0300

    :tada: Configuração inicial e criação do projeto

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+
+export default {
+
+  content: [
+
+    "./index.html",
+
+    "./src/**/*.{js,ts,jsx,tsx}", // Inclui todos os arquivos JS/TS/JSX/TSX dentro de src
+
+  ],
+
+  theme: {
+
+    extend: {},
+
+  },
+
+  plugins: [],
+
+}
